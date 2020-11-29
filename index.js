ad58ec489057a1061dd28d4b68cffeb84ce5dc67
Danny Maier <dannymaier@gmail.com>
2020-11-28 18:43:00 -0800

    I have no idea what i'm doing but i added some of the functions from Day 2. I created a template for the generatemarkdown file and imported it to index. in index i have some functionality to prompt the user some questions and return the input to write a file. i still need to figure out how to connect the user input in index, to the template in generatemarkdown.

--- index.js
+++ index.js
@@ -1,16 +1,110 @@
 // array of questions for user
 const questions = [
+    "0. WHAT IS YOUR NAME, punk", 
+    "1. WHAT IS YOUR EMAIL ADDRESS",
+    "2. WHAT IS YOUR PROJECT'S NAME",
+    "3. PLEASE WRITE A SHORT DESCRIPTION OF YOUR PROJECT", 
+    "4. WHAT COMMAND SHOULD BE RUN TO INSTALL DEPENDENCIES (NPM I)",
+    "5. WHAT COMMAND SHOULD BE RUN TO RUN TESTS (NPM TEST)",
+    "6. WHAT DOES THE USER NEED TO KNOW ABOUT USING THE REPO?",
+    "7. WHAT DOES THE USER NEED TO KNOW ABOUT CONTRIBUTING TO THE REPO",
 
 ];
+    // console.log(questions[0])
+
+//REQUIRED MODULES
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown.js')
 
 // function to write README file
-function writeToFile(fileName, data) {
-}
+
+
+inquirer
+    .prompt([
+    {
+        type: 'input',
+        name: 'question0',
+        message: `${questions[0]}`,
+    },
+    {
+        type: 'input',
+        name: 'question1',
+        message: `${questions[1]}`,
+    },
+    ]).then((data) => {
+        console.log(data)
+    //DELETE THE JUNK BELOW
+        // function writeToFile(fileName, data) {
+        //     fs.writeFile('test.txt', data, (err))
+        // }
+    //DELETE THE JUNK ABOVE
+        fs.writeFile('test.json', JSON.stringify(data), (err) => {
+            err?
+            console.log('failed to write file'):
+            console.log('wrote file')
+        })
+});
 
 // function to initialize program
 function init() {
-
 }
 
 // function call to initialize program
 init();
+
+
+
+
+
+//RESOURCES BELOW
+
+    //ATTEMPT WITH A FOR LOOP
+// for (let i = 0; i < questions.length; i++) {
+//     const element = questions[i];
+    
+    
+
+// inquirer
+//     .prompt(
+//     {
+//         type: 'input',
+//         name: 'name',
+//         message: `${questions[i]}`,
+//     }
+//     ).then((data) => {
+//         const filename = `${data.name.toLowerCase().split(' ').join('')}.json`;
+    
+//         fs.writeFile(filename, JSON.stringify(data, null, '\t'), (err) =>
+//         err ? 
+//         console.log(err) : 
+//         console.log('Success!')
+//         );
+//         });
+// }
+
+    //STOCK FROM EXERCISE 20
+// inquirer
+//     .prompt([
+//     {
+//         type: 'checkbox',
+//         message: 'What languages do you know?',
+//         name: 'stack',
+//         choices: ['HTML', 'CSS', 'JavaScript', 'MySQL'],
+//     },
+//     {
+//         type: 'list',
+//         message: 'What is your preferred method of communication?',
+//         name: 'contact',
+//         choices: ['email', 'phone', 'telekinesis'],
+//     },
+//     ])
+    // .then((data) => {
+    // const filename = `${data.name.toLowerCase().split(' ').join('')}.json`;
+
+    // fs.writeFile(filename, JSON.stringify(data, null, '\t'), (err) =>
+    // err ? 
+    // console.log(err) : 
+    // console.log('Success!')
+    // );
+    // });
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,8 +1,37 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+
+  return `# ${data.title};
+  
+  ${generateDescription(data.description)}
+  ## Table of Contents
+
+  *[Installation](#installation)
+
+  *[Usage](#usage)
+
+  *[License](#license)]
+
+  *[Contributing](#contributing)
+
+  *[Tests](#tests)
+
+  *[Questions](#questions)
 
 `;
 }
 
+function generateDescription (description){
+  return `## Description 
+  ${description}`;
+}
+
+console.log(generateMarkdown({
+  title: "testing",
+  description:"Testing description"
+}));
+
+
 module.exports = generateMarkdown;
+
+
